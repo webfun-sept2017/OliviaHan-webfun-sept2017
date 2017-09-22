e9ee7ea37399ec6ce7a34d9184782eabd25c6f0c
Olivia0114 <Olivia.han0114@gmail.com>
2017-09-22 00:22:13 -0700

    added javascript

---
+++ I Do Declare/delcare.js
@@ -0,0 +1,21 @@
+var number1 = 100;
+var number2 = -10;
+var number3 =2.5;
+var number4 = 250;
+var name = "Olivia";
+var occupation = "Program Management";
+var company = "beyondsoft"
+var male = true;
+var famale = false;
+var description
+
+console.log("hundred write in number is ", number1);
+console.log("how to display negative number ", number2);
+console.log("how about decimal point ", number3);
+console.log("a phrase in chinese ", number4);
+console.log("My english name is ", name);
+console.log("What do you do? ", occupation);
+console.log("Where do you work", company);
+console.log("Is Steven male", male);
+console.log("Is Steven female," female);
+console.log("what undefined variable will show?" description);
